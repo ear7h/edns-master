3a0425625240c281bf70fafc533f7438136eb2bd
Julio <julio.grillo98@gmail.com>
2017-11-09 16:37:48 -0800

    testing the slave node, use fatter image for docker to debug

---
+++ slave/slave_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+	"github.com/miekg/dns"
+	"fmt"
+)
+
+func TestPost(t *testing.T) {
+	_localServices["test-service"] = "127.0.0.1:8080"
+
+	post()
+
+	m := new(dns.Msg)
+	m.SetQuestion("test-service.ear7h.net.", dns.TypeCNAME)
+
+	r, err := dns.Exchange(m, _masterHost+":53")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(r)
+}
